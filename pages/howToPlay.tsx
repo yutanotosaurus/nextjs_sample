33a2c9ff35c3d4c58bcbe8c01a2e1552ab7ca02e
RimlTempest <riml.slime@gmail.com>
2021-09-21 00:28:44 +0900

    fix: lintを適用

--- pages/howToPlay.tsx
+++ pages/howToPlay.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-const howToPlay: NextPage = () =>{
+const howToPlay: NextPage = () => {
     return (
         <div className={styles.container}>
             <Head>
@@ -10,15 +10,11 @@ const howToPlay: NextPage = () =>{
                 <meta name="description" content="クイズ画面遊び方画面" />
             </Head>
             <main className={styles.main}>
-                <h1 className={styles.title}>
-                    クイズの説明画面
-                </h1>
-                <p className={styles.description}>
-                    クイズ説明ここに書く
-                </p>
+                <h1 className={styles.title}>クイズの説明画面</h1>
+                <p className={styles.description}>クイズ説明ここに書く</p>
             </main>
             <footer className={styles.footer}>
-                <p>Powered by{' '} Yuta</p>
+                <p>Powered by Yuta</p>
             </footer>
         </div>
     )
