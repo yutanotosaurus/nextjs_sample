dc4d4cc187a7c3d7ccacc32f62ba478471cf3b79
RimlTempest <riml.slime@gmail.com>
2021-09-22 02:31:47 +0900

    update: 取得データをキャッシュするように更新

--- pages/quiz.tsx
+++ pages/quiz.tsx
@@ -2,16 +2,21 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import axios from 'axios'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-const Quiz: NextPage = () =>{
-    const [data, setData] = useState('');
-    const url = "/quiz.json";
-    axios.get(url).then(res => {
-        //console.log(res.data);
-        setData(res.data);
-      });
-    return(
+const Quiz: NextPage = () => {
+    const [data, setData] = useState('')
+    const url = '/quiz.json'
+    // custom hook化する
+    useEffect(() => {
+        async function fetchQuiz() {
+            const res = await axios(url)
+            setData(res.data)
+        }
+        fetchQuiz()
+    }, [])
+    console.log(data)
+    return (
         <div className={styles.container}>
             <Head>
                 <title>クイズ画面</title>
